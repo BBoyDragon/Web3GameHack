6eafe97b7c82a3d34d357e13026bfd8e5d20386e
Ismaxis <Ismaxis@github.com>
2023-03-14 01:53:44 +0300

    + Maze spawn added

---
+++ Web3Labirint/Assets/Code/Maze/Spawn/MazeRender.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeRender : MonoBehaviour
+{
+    [SerializeField] GameObject mazeCellPrefab;
+
+    public float CellSize = 1.0f;
+
+    private void Start()
+    {
+        Maze maze = MazeGenerator.KruskalGenerate(20, 20);
+
+        for (int x = 0; x < maze.SizeX(); x++)
+        {
+            for (int y = 0; y < maze.SizeY(); y++)
+            {
+                GameObject newCell = Instantiate(mazeCellPrefab, new Vector3(x * CellSize, .0f, (maze.SizeY() - y) * CellSize), Quaternion.identity, transform);
+                MazeCellObject mazeCell = newCell.GetComponent<MazeCellObject>();
+
+                bool top = y == 0;
+                bool bottom = maze.IsHorizontalWall(x + 1, y + 1);
+                bool left = x == 0;
+                bool right = maze.IsVerticalWall(x + 1, y + 1);
+
+                mazeCell.Init(top, bottom, left, right);
+            }
+        }
+    }
+}
